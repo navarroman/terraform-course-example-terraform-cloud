3fc030f5bc42af0138a1d79aee3c67e59ec20b19
Carlos Navarro <carlos.navarroman@icloud.com>
2025-12-16 11:48:29 -0600

    feat(networking): add subnet

--- networking.tf
+++ networking.tf
@@ -5,3 +5,14 @@ resource "aws_vpc" "tf_cloud" {
     Name = "terraform-cloud"
   }
 }
+
+resource "aws_subnet" "tf_cloud" {
+  vpc_id            = aws_vpc.tf_cloud.id
+  cidr_block        = var.subnet_cidr
+  availability_zone = "us-east-1a"
+
+  tags = {
+    "Name" = "tf_cloud_subnet"
+  }
+
+}
